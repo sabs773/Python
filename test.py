f782ac3b3d9923b8c108310cc2f53f2878551ba3
Sabs773 <sabsikhwan@gmail.com>
2021-02-06 19:37:47 +0800

    New changes

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 
+# identation is 2 tab spaces (up to programmer but stick 2 tab spaces)
+
 # When unpacking lists make sure the number of variables = number of elements
 
 fruits = ["apple", "banana", "cherry"]
@@ -7,4 +9,12 @@ x, y , z = fruits
 print(x)
 print(y)
 print(z)
+
+# adding together number and string types without casting throws an error
+# Cast only when it is necessary and there is no other choice 
+
+x = str(5)
+y = " John"
+print(x + y)
+
 
