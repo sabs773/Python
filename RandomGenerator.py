376906e309fbdb421843519bdd25dd5e13d327d7
Sabs773 <sabsikhwan@gmail.com>
2021-02-27 22:38:54 +0800

    //

--- RandomGenerator.py
+++ RandomGenerator.py
@@ -1,7 +1,23 @@
 import numpy as np
 
-arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+# arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
 
-print('2nd element on 1st dim: ', arr[0,0,2])
+# print('2nd element on 1st dim: ', arr[0,0,2])
+
+#slicing 2-D arrays
+# arr = np.array([1, 2, 3,4,5,6,7])
+
+# print(arr[::2])
+
+# arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+
+# print(arr[0:2,1:4])
+
+#slicing 3-D arrays with
+
+# arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+# print(arr[0:2,0:2,0:2])
+
 
 
---
+++ exercise1.py
@@ -0,0 +1,7 @@
+import numpy as np 
+
+# convert a list of numeric value into a one-dimensional NumPy array
+
+list = [12.23,13.32,"abbie",36.32]
+
+print("original list",list)
